fcfcd09df0f66cd7a2b194c42b0354fc570a9da2
Steve Mao <maochenyan@gmail.com>
2017-02-18 15:20:42 +1100

    fix error

--- app.py
+++ app.py
@@ -40,3 +40,10 @@ def webhook():
 
 def processRequest(req):
     return chatbot.get_response(req)
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
