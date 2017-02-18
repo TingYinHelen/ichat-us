683355cce857b285afaacb487f897ac973afad90
Steve Mao <maochenyan@gmail.com>
2017-02-18 15:31:54 +1100

    print response json

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ def webhook():
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    # print(res)
+    print("Response json:")
+    print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
